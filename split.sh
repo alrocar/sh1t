1a022958e453ac5d5b217898d0cb2576eef87ff8
Alberto Romeu <alrocar@cartodb.com>
2017-06-05 12:21:25 +0200

    Add arguments

--- split.sh
+++ split.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
-tail -n +2 file.txt | split -l 10000 - split_
+# usage ./split.sh filename number_of_lines
+tail -n +2 $1 | split -l $2 - split_
 for file in split_*
 do
-    head -n 1 file.txt > tmp_file
+    head -n 1 $1 > tmp_file
     cat $file >> tmp_file
-    mv -f tmp_file $file
+    mv -f tmp_file $file.csv
+    rm $file
 done
